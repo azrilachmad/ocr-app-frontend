0b080bdb4f1aba45e5f16fcd1bbb49951ddcc75b
Azril Achmad <azrilachmad4@gmail.com>
2026-02-22 16:51:41 +0700

    feat: Implement core OCR application with document processing, administration, user management, and chat functionalities across frontend and backend.

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -3,13 +3,25 @@ const User = require('./User');
 const Document = require('./Document');
 const DocumentType = require('./DocumentType');
 const Settings = require('./Settings');
+const SystemConfig = require('./SystemConfig');
+const ChatSession = require('./ChatSession');
+const ChatMessage = require('./ChatMessage');
 
 // Define associations
 User.hasMany(Document, { foreignKey: 'userId', as: 'documents' });
 Document.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
+User.hasMany(DocumentType, { foreignKey: 'userId', as: 'documentTypes' });
+DocumentType.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
 User.hasOne(Settings, { foreignKey: 'userId', as: 'settings' });
 Settings.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+User.hasMany(ChatSession, { foreignKey: 'userId', as: 'chatSessions' });
+ChatSession.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+ChatSession.hasMany(ChatMessage, { foreignKey: 'sessionId', as: 'messages' });
+ChatMessage.belongsTo(ChatSession, { foreignKey: 'sessionId', as: 'session' });
 
 // Sync all models
 const syncModels = async (force = false) => {
@@ -27,5 +39,8 @@ module.exports = {
     Document,
     DocumentType,
     Settings,
+    SystemConfig,
+    ChatSession,
+    ChatMessage,
     syncModels
 };
